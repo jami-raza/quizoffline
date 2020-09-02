c6448e7151725b5d7f8b8729db556790d90f9c36
jami-raza <57630996+jami-raza@users.noreply.github.com>
2020-09-02 09:51:52 +0500

    lets start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets start quiz app with offline availability</h1>
+    </div>
+  );
+}
+
+export default App;
