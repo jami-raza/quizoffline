81fe51bb7cf2e9fca9edd198aeca4b9ff775f255
jami-raza <57630996+jami-raza@users.noreply.github.com>
2020-09-04 02:08:39 +0500

    update domain

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/7.19.1/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/7.19.1/firebase-messaging.js')
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCCZQ1uE8h_HsYTONYd4xEemSlvnxU1-qw",
-    authDomain: "messaging-app-b361a.firebaseapp.com",
-    databaseURL: "https://messaging-app-b361a.firebaseio.com",
-    projectId: "messaging-app-b361a",
-    storageBucket: "messaging-app-b361a.appspot.com",
-    messagingSenderId: "803863034285",
-    appId: "1:803863034285:web:2c8e8e98c5165cb0e40924"
+    apiKey: "AIzaSyCcF_nkZR7s3feXCHQvSEoGsQ3KJvLLau4",
+    authDomain: "quiz-general-knowledge-5c2e0.firebaseapp.com",
+    databaseURL: "https://quiz-general-knowledge-5c2e0.firebaseio.com",
+    projectId: "quiz-general-knowledge-5c2e0",
+    storageBucket: "quiz-general-knowledge-5c2e0.appspot.com",
+    messagingSenderId: "587388719554",
+    appId: "1:587388719554:web:e01b651829b3ac7c08465b"
 }
 firebase.initializeApp(firebaseConfig)
 firebase.messaging();
